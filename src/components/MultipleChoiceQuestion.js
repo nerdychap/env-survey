d77db817ca753726e01683ab7b8764c3adddc1f9
siya-h <siyabonga.hlengwa@dotmodus.com>
2022-09-12 21:17:47 +0200

    changong background color and fixing uncontrolled component change error

--- src/components/MultipleChoiceQuestion.js
+++ src/components/MultipleChoiceQuestion.js
@@ -1,10 +1,14 @@
 import { Box, Card, FormControl, MenuItem, Select } from '@mui/material'
-import React, { useId } from 'react'
+import React, { useId, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import QuestionButtons from './QuestionButtons'
 
 const MultipleChoiceQuestion = ({ question, options, id, answerType }) => {
     const elementId = useId()
+    const [value, setValue] = useState('')
+    const handleChange = ({ target: { value } }) => {
+        setValue(value)
+    }
     return (
         <Card sx={{ p: 2, m: 1 }}>
 
@@ -14,6 +18,8 @@ const MultipleChoiceQuestion = ({ question, options, id, answerType }) => {
                     <Select
                         labelId={elementId}
                         id={elementId}
+                        value={value}
+                        onChange={handleChange}
                     >
                         {
                             options.map((item) => <MenuItem key={uuidv4()} value={item}>{item}</MenuItem>)
